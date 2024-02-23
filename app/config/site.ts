0d88019245c39085da643e73f3d15298790a8392
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-02-23 15:08:53 -0800

    created more ui components and a header

---
+++ app/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "DrakeAlia/Book Reviews",
+  description:
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+  links: {
+    twitter: "https://twitter.com/drake___alia",
+    github: "http://www.github.com/DrakeAlia",
+    githubProject: "http://www.github.com/DrakeAlia/book-reviews",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
