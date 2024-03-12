00cc537905c4b92589a30b19b262e8c6a7fc15d6
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-03-11 20:44:15 -0700

    made some adjustments to prisma for certain tables

--- auth.ts
+++ auth.ts
@@ -6,8 +6,6 @@ import { db } from "@/db";
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
-console.log("GITHUB_CLIENT_ID", GITHUB_CLIENT_ID);
-console.log("GITHUB_CLIENT_SECRET", GITHUB_CLIENT_SECRET);
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
   throw new Error("Missing github oauth credentials");
---
+++ prisma/migrations/20240312033712_added_account_table/migration.sql
@@ -0,0 +1,101 @@
+/*
+  Warnings:
+
+  - You are about to drop the `UserBookList` table. If the table is not empty, all the data it contains will be lost.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - The primary key for the `Book` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `publishedAt` on the `Book` table. All the data in the column will be lost.
+  - The primary key for the `Review` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `comment` on the `Review` table. All the data in the column will be lost.
+  - Added the required column `cover` to the `Book` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Review` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "UserBookList";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL,
+    CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "BookList" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "bookId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "BookList_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "BookList_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerifiedAt" DATETIME,
+    "image" TEXT
+);
+INSERT INTO "new_User" ("email", "id", "name") SELECT "email", "id", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE TABLE "new_Book" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "cover" TEXT NOT NULL
+);
+INSERT INTO "new_Book" ("author", "id", "title") SELECT "author", "id", "title" FROM "Book";
+DROP TABLE "Book";
+ALTER TABLE "new_Book" RENAME TO "Book";
+CREATE TABLE "new_Review" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "bookId" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "content" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Review_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Review" ("bookId", "id", "rating", "userId") SELECT "bookId", "id", "rating", "userId" FROM "Review";
+DROP TABLE "Review";
+ALTER TABLE "new_Review" RENAME TO "Review";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
