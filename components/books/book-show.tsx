b72042279d6b5c4e04aa1f3f41e1d08c5b3cb061
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-04-29 15:18:42 -0700

    fixed bookShow

--- components/books/book-show.tsx
+++ components/books/book-show.tsx
@@ -15,24 +15,21 @@ export default async function BookShow({ bookId }: BookShowProps) {
 
     console.log("Fetching:", book);
 
-    //   if (!book) {
-    //     console.error("No book found with ID:", bookId);
-    //     return notFound();
-    //   }
-
-    console.log("Book details:", book);
-    console.log("Book title:", book.title);
-    console.log("Book author:", book.author);
-    console.log("Book genre:", book.genre);
-    console.log("Book description:", book.description);
+    if (!book) {
+      console.error("No book found with ID:", bookId);
+      return notFound();
+    }
 
     return (
-      <div className="m-4 pt-4">
-        <h1 className="text-2xl font-bold my-2">
-          {book.title} by {book.author}
-        </h1>
-        <p className="text-lg text-gray-500 my-2">{book.genre}</p>
-        <p className="p-4 border rounded-md">{book.description}</p>
+      <div>
+        {book && (
+          <>
+            <h1>{book.title}</h1>
+            <p>Author: {book.author}</p>
+            <p>Genre: {book.genre}</p>
+            <p>Description: {book.description}</p>
+          </>
+        )}
       </div>
     );
   } catch (error) {
