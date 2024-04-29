271f59b96e6fb15e890d212a0272e46bb8e21ab8
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-04-29 15:22:15 -0700

    removed comments

--- app/bookshelf/books/[bookId]/page.tsx
+++ app/bookshelf/books/[bookId]/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import paths from "@/paths";
 import BookShow from "@/components/books/book-show";
 import BookShowLoading from "@/components/books/book-show-loading";
 import { Suspense } from "react";
@@ -14,12 +12,6 @@ export default function BookShowPage({ params }: BookShowPageProps) {
   return (
     <section className="container px-6 pt-8 md:pt-12">
       <div className="space-y-3">
-        {/* <Link
-          className="underline decoration-solid"
-          href={paths.bookShow(bookId)}
-        >
-          Back to Books
-        </Link> */}
         <Suspense fallback={<BookShowLoading />}>
           <BookShow bookId={bookId} />
         </Suspense>
@@ -27,19 +19,3 @@ export default function BookShowPage({ params }: BookShowPageProps) {
     </section>
   );
 }
-
-// interface BookShowPageProps {
-//   params: { bookId: string; reviewId: string | null };
-// }
-
-// export default function BookShowPage({ params }: BookShowPageProps) {
-//   const { reviewId } = params;
-
-//   if (reviewId) {
-//     console.log("Review ID is present:", reviewId);
-//   } else {
-//     console.log("Review ID is missing or undefined");
-//   }
-
-//   return null;
-// }
